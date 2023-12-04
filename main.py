500fc2f01bfd741ff36d4633c8f4935cf77ec04b
Rávilla <148885857+ravsnoely1@users.noreply.github.com>
2023-12-04 14:47:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from multi_tape_turing_machine import *
 import json
+import sys
 
-# CEFET/LFA/maquina_turing.json "001100001"
-
+# maquina_turing.json "001100001"
 def main():
-    # Carregar a especificação da máquina de Turing a partir de um arquivo JSON
-    entradas = input()
-    dados = entradas.split()
-    
-    if len(dados) != 2:
+    if len(sys.argv) != 3:
         print("Usar: python3 main.py [MT] [Palavra]")
-    else:
-        arquivo, palavra = dados
+        sys.exit(1)
+  
+    arquivo = sys.argv[1]
+    palavra = sys.argv[2]
 
-        with open(arquivo, "r") as arquivo_json:
-            # Lendo e desserializando o conteúdo do arquivo JSON
-            json_data = json.load(arquivo_json)
+    with open(arquivo, "r") as arquivo:
+        # Lendo e desserializando o conteúdo do arquivo JSON
+        json_data = json.load(arquivo)
 
-        # Inicializar a máquina de Turing
-        mt = MultiTapeTuringMachine()
-        mt.load_from_json(json_data)
+    # Inicializar a máquina de Turing
+    mt = MultiTapeTuringMachine()
+    mt.load_from_json(json_data)
 
-        # Executar a máquina de Turing com a palavra de entrada
-        resultado = mt.run(palavra)
+    # Executar a máquina de Turing com a palavra de entrada
+    resultado = mt.run(palavra)
 
-        # Exibir o resultado
-        print(resultado)
+    # Exibir o resultado
+    print(resultado)
 
 if __name__ == "__main__":
     main()
